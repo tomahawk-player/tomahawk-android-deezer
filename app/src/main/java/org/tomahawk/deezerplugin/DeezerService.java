14683a46fd194f9d1d4373cd068ff90ea65e3186
Enno Gottschalk <mrmaffen@googlemail.com>
2016-01-22 20:57:43 +0100

    Use Messenger instead of direct AIDL. Bumped version to 0.20.

--- app/src/main/java/org/tomahawk/deezerplugin/DeezerService.java
+++ app/src/main/java/org/tomahawk/deezerplugin/DeezerService.java
@@ -27,18 +27,18 @@ import com.deezer.sdk.player.event.PlayerState;
 import com.deezer.sdk.player.exception.TooManyPlayersExceptions;
 import com.deezer.sdk.player.networkcheck.WifiAndMobileNetworkStateChecker;
 
-import org.tomahawk.aidl.IPluginService;
-import org.tomahawk.aidl.IPluginServiceCallback;
-
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
-import android.os.RemoteCallbackList;
+import android.os.Message;
+import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.concurrent.RejectedExecutionException;
 
 public class DeezerService extends Service {
@@ -46,120 +46,186 @@ public class DeezerService extends Service {
     // Used for debug logging
     private static final String TAG = DeezerService.class.getSimpleName();
 
+    /**
+     * Command to the service to register a client, receiving callbacks from the service. The
+     * Message's replyTo field must be a Messenger of the client where callbacks should be sent.
+     */
+    static final int MSG_REGISTER_CLIENT = 1;
+
+    /**
+     * Command to the service to unregister a client, ot stop receiving callbacks from the service.
+     * The Message's replyTo field must be a Messenger of the client as previously given with
+     * MSG_REGISTER_CLIENT.
+     */
+    static final int MSG_UNREGISTER_CLIENT = 2;
+
+    /**
+     * Commands to the service
+     */
+    private static final int MSG_PREPARE = 100;
+
+    private static final String MSG_PREPARE_ARG_URI = "uri";
+
+    private static final String MSG_PREPARE_ARG_ACCESSTOKEN = "accessToken";
+
+    private static final String MSG_PREPARE_ARG_ACCESSTOKENEXPIRES = "accessTokenExpires";
+
+    private static final int MSG_PLAY = 101;
+
+    private static final int MSG_PAUSE = 102;
+
+    private static final int MSG_SEEK = 103;
+
+    private static final String MSG_SEEK_ARG_MS = "ms";
+
+    private static final int MSG_SETBITRATE = 104;
+
+    private static final String MSG_SETBITRATE_ARG_MODE = "mode";
+
+    /**
+     * Commands to the client
+     */
+    private static final int MSG_ONPAUSE = 200;
+
+    private static final int MSG_ONPLAY = 201;
+
+    private static final int MSG_ONPREPARED = 202;
+
+    protected static final String MSG_ONPREPARED_ARG_URI = "uri";
+
+    private static final int MSG_ONPLAYERENDOFTRACK = 203;
+
+    private static final int MSG_ONPLAYERPOSITIONCHANGED = 204;
+
+    private static final String MSG_ONPLAYERPOSITIONCHANGED_ARG_POSITION = "position";
+
+    private static final String MSG_ONPLAYERPOSITIONCHANGED_ARG_TIMESTAMP = "timestamp";
+
+    private static final int MSG_ONERROR = 205;
+
+    private static final String MSG_ONERROR_ARG_MESSAGE = "message";
+
     public final static String APP_ID = "";
 
     private TrackPlayer mPlayer;
 
-    /**
-     * This is a list of callbacks that have been registered with the service.  Note that this is
-     * package scoped (instead of private) so that it can be accessed more efficiently from inner
-     * classes.
-     */
-    final RemoteCallbackList<IPluginServiceCallback> mCallbacks = new RemoteCallbackList<>();
+    private String mPreparedUri;
 
     /**
-     * The IRemoteInterface is defined through IDL
+     * Keeps track of all current registered clients.
      */
-    private final IPluginService.Stub mBinder = new IPluginService.Stub() {
+    ArrayList<Messenger> mClients = new ArrayList<>();
 
-        @Override
-        public void registerCallback(IPluginServiceCallback cb) {
-            if (cb != null) {
-                mCallbacks.register(cb);
-            }
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler(this));
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    private static class IncomingHandler extends WeakReferenceHandler<DeezerService> {
+
+        public IncomingHandler(DeezerService referencedObject) {
+            super(referencedObject);
         }
 
         @Override
-        public void unregisterCallback(IPluginServiceCallback cb) {
-            if (cb != null) {
-                mCallbacks.unregister(cb);
-            }
-        }
+        public void handleMessage(Message msg) {
+            final DeezerService s = getReferencedObject();
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    s.mClients.add(msg.replyTo);
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    s.mClients.remove(msg.replyTo);
+                    break;
+                case MSG_PREPARE:
+                    String uri = msg.getData().getString(MSG_PREPARE_ARG_URI);
+                    String accessToken =
+                            msg.getData().getString(MSG_PREPARE_ARG_ACCESSTOKEN);
+                    long accessTokenExpires =
+                            msg.getData().getLong(MSG_PREPARE_ARG_ACCESSTOKENEXPIRES);
 
-        @Override
-        public void prepare(String uri, String accessToken, String accessTokenSecret,
-                long accessTokenExpires) throws RemoteException {
-            DeezerConnect deezerConnect = new DeezerConnect(APP_ID);
-            deezerConnect.setAccessToken(getApplicationContext(), accessToken);
-            deezerConnect.setAccessExpires(accessTokenExpires);
-            try {
-                if (mPlayer == null
-                        || mPlayer.getPlayerState() == PlayerState.RELEASED) {
-                    mPlayer = new TrackPlayer(getApplication(), deezerConnect,
-                            new WifiAndMobileNetworkStateChecker());
-                    mPlayer.addOnBufferErrorListener(mPlayerHandler);
-                    mPlayer.addOnPlayerErrorListener(mPlayerHandler);
-                    mPlayer.addOnPlayerStateChangeListener(mPlayerHandler);
-                }
-            } catch (TooManyPlayersExceptions | DeezerError e) {
-                Log.e(TAG, "<init>: " + e.getClass() + ": " + e
-                        .getLocalizedMessage());
-            }
-            synchronized (this) {
-                mPlayer.playTrack(Long.valueOf(uri));
-            }
-        }
+                    s.mPreparedUri = uri;
 
-        @Override
-        public void play() throws RemoteException {
-            if (mPlayer != null && mPlayer.getPlayerState() != PlayerState.RELEASED) {
-                synchronized (this) {
-                    if (mPlayer.getPlayerState() != PlayerState.WAITING_FOR_DATA
-                            && mPlayer.getPlayerState() != PlayerState.STOPPED) {
-                        mPlayer.play();
+                    DeezerConnect deezerConnect = new DeezerConnect(APP_ID);
+                    deezerConnect.setAccessToken(s.getApplicationContext(), accessToken);
+                    deezerConnect.setAccessExpires(accessTokenExpires);
+                    try {
+                        if (s.mPlayer == null
+                                || s.mPlayer.getPlayerState() == PlayerState.RELEASED) {
+                            s.mPlayer = new TrackPlayer(s.getApplication(), deezerConnect,
+                                    new WifiAndMobileNetworkStateChecker());
+                            s.mPlayer.addOnBufferErrorListener(s.mPlayerHandler);
+                            s.mPlayer.addOnPlayerErrorListener(s.mPlayerHandler);
+                            s.mPlayer.addOnPlayerStateChangeListener(s.mPlayerHandler);
+                        }
+                    } catch (TooManyPlayersExceptions | DeezerError e) {
+                        Log.e(TAG, "<init>: " + e.getClass() + ": " + e
+                                .getLocalizedMessage());
                     }
-                }
-            }
-        }
-
-        @Override
-        public void pause() throws RemoteException {
-            if (mPlayer != null && mPlayer.getPlayerState() != PlayerState.RELEASED) {
-                synchronized (this) {
-                    if (mPlayer.getPlayerState() != PlayerState.WAITING_FOR_DATA
-                            && mPlayer.getPlayerState() != PlayerState.STOPPED) {
-                        mPlayer.pause();
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void seek(final int ms) throws RemoteException {
-            if (mPlayer != null && mPlayer.getPlayerState() != PlayerState.RELEASED) {
-                try {
                     synchronized (this) {
-                        try {
-                            mPlayer.seek(ms);
-                        } catch (Exception e) {
-                            Log.e(TAG, "seekTo: " + e.getClass() + ": " + e.getLocalizedMessage());
-                            broadcastToAll(new BroadcastRunnable() {
-                                @Override
-                                public void broadcast(IPluginServiceCallback callback)
-                                        throws RemoteException {
-                                    callback.onError("Error while seeking");
-                                }
-                            });
+                        s.mPlayer.playTrack(Long.valueOf(uri));
+                    }
+                    break;
+                case MSG_PLAY:
+                    Log.d(TAG, "play called");
+                    if (s.mPlayer != null && s.mPlayer.getPlayerState() != PlayerState.RELEASED) {
+                        synchronized (this) {
+                            if (s.mPlayer.getPlayerState() != PlayerState.WAITING_FOR_DATA
+                                    && s.mPlayer.getPlayerState() != PlayerState.STOPPED) {
+                                s.mPlayer.play();
+                            }
                         }
                     }
-
-                    broadcastToAll(new BroadcastRunnable() {
-                        @Override
-                        public void broadcast(IPluginServiceCallback callback)
-                                throws RemoteException {
-                            callback.onPlayerPositionChanged(ms, System.currentTimeMillis());
+                    break;
+                case MSG_PAUSE:
+                    Log.d(TAG, "pause called");
+                    if (s.mPlayer != null && s.mPlayer.getPlayerState() != PlayerState.RELEASED) {
+                        synchronized (this) {
+                            if (s.mPlayer.getPlayerState() != PlayerState.WAITING_FOR_DATA
+                                    && s.mPlayer.getPlayerState() != PlayerState.STOPPED) {
+                                s.mPlayer.pause();
+                            }
                         }
-                    });
-                } catch (RejectedExecutionException e) {
-                    Log.e(TAG, "seek - " + e.getLocalizedMessage());
-                }
+                    }
+                    break;
+                case MSG_SEEK:
+                    int ms = msg.getData().getInt(MSG_SEEK_ARG_MS);
+
+                    Log.d(TAG, "seek()");
+                    if (s.mPlayer != null && s.mPlayer.getPlayerState() != PlayerState.RELEASED) {
+                        try {
+                            synchronized (this) {
+                                try {
+                                    s.mPlayer.seek(ms);
+                                } catch (Exception e) {
+                                    Log.e(TAG, "seekTo: " + e.getClass() + ": " + e
+                                            .getLocalizedMessage());
+                                    Bundle args = new Bundle();
+                                    args.putString(MSG_ONERROR_ARG_MESSAGE, "Error while seeking");
+                                    s.broadcastToAll(MSG_ONERROR, args);
+                                }
+                            }
+
+                            Bundle args = new Bundle();
+                            args.putInt(MSG_ONPLAYERPOSITIONCHANGED_ARG_POSITION, ms);
+                            args.putLong(MSG_ONPLAYERPOSITIONCHANGED_ARG_TIMESTAMP,
+                                    System.currentTimeMillis());
+                            s.broadcastToAll(MSG_ONPLAYERPOSITIONCHANGED, args);
+                        } catch (RejectedExecutionException e) {
+                            Log.e(TAG, "seek - " + e.getLocalizedMessage());
+                        }
+                    }
+                    break;
+                case MSG_SETBITRATE:
+                    break;
+                default:
+                    super.handleMessage(msg);
             }
         }
-
-        @Override
-        public void setBitRate(int mode) throws RemoteException {
-        }
-    };
+    }
 
     private final PlayerHandler mPlayerHandler = new PlayerHandler();
 
@@ -172,13 +238,9 @@ public class DeezerService extends Service {
                 @Override
                 public void run() {
                     Log.d(TAG, "onBufferError: ", ex);
-                    broadcastToAll(new BroadcastRunnable() {
-                        @Override
-                        public void broadcast(IPluginServiceCallback callback)
-                                throws RemoteException {
-                            callback.onError("Error while buffering");
-                        }
-                    });
+                    Bundle args = new Bundle();
+                    args.putString(MSG_ONERROR_ARG_MESSAGE, "Error while buffering");
+                    broadcastToAll(MSG_ONERROR, args);
                 }
             });
         }
@@ -189,13 +251,10 @@ public class DeezerService extends Service {
                 @Override
                 public void run() {
                     Log.d(TAG, "onPlayerError: ", ex);
-                    broadcastToAll(new BroadcastRunnable() {
-                        @Override
-                        public void broadcast(IPluginServiceCallback callback)
-                                throws RemoteException {
-                            callback.onError("PlayerError: " + ex.getLocalizedMessage());
-                        }
-                    });
+                    Bundle args = new Bundle();
+                    args.putString(MSG_ONERROR_ARG_MESSAGE,
+                            "PlayerError: " + ex.getLocalizedMessage());
+                    broadcastToAll(MSG_ONERROR, args);
                 }
             });
         }
@@ -205,29 +264,20 @@ public class DeezerService extends Service {
             new Handler(Looper.getMainLooper()).post(new Runnable() {
                 @Override
                 public void run() {
-                    broadcastToAll(new BroadcastRunnable() {
-                        @Override
-                        public void broadcast(IPluginServiceCallback callback)
-                                throws RemoteException {
-                            if (state == PlayerState.READY) {
-                                callback.onPrepared();
-                            } else if (state == PlayerState.PLAYBACK_COMPLETED) {
-                                callback.onPlayerEndOfTrack();
-                            } else if (state == PlayerState.PAUSED) {
-                                callback.onPause();
-                            } else if (state == PlayerState.STARTED) {
-                                callback.onPlay();
-                            }
-                        }
-                    });
+                    if (state == PlayerState.READY) {
+                        Bundle args = new Bundle();
+                        args.putString(MSG_ONPREPARED_ARG_URI, mPreparedUri);
+                        broadcastToAll(MSG_ONPREPARED, args);
+                    } else if (state == PlayerState.PLAYBACK_COMPLETED) {
+                        broadcastToAll(MSG_ONPLAYERENDOFTRACK);
+                    } else if (state == PlayerState.PAUSED) {
+                        broadcastToAll(MSG_ONPAUSE);
+                    } else if (state == PlayerState.STARTED) {
+                        broadcastToAll(MSG_ONPLAY);
+                    }
                 }
             });
         }
-    }
-
-    interface BroadcastRunnable {
-
-        void broadcast(IPluginServiceCallback callback) throws RemoteException;
     }
 
     @Override
@@ -239,7 +289,6 @@ public class DeezerService extends Service {
 
     @Override
     public void onDestroy() {
-        mCallbacks.kill();
         if (mPlayer != null) {
             mPlayer.release();
         }
@@ -248,33 +297,38 @@ public class DeezerService extends Service {
         super.onDestroy();
     }
 
+    /**
+     * When binding to the service, we return an interface to our messenger for sending messages to
+     * the service.
+     */
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "Client has been bound to DeezerService");
-        if (IPluginService.class.getName().equals(intent.getAction())) {
-            return mBinder;
-        }
-        return null;
+        return mMessenger.getBinder();
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
         Log.d(TAG, "Client has been unbound from DeezerService");
-        stopSelf();
         return false;
     }
 
-    private void broadcastToAll(BroadcastRunnable runnable) {
-        // Broadcast to all clients
-        final int N = mCallbacks.beginBroadcast();
-        for (int i = 0; i < N; i++) {
+    private void broadcastToAll(int what) {
+        broadcastToAll(what, null);
+    }
+
+    private void broadcastToAll(int what, Bundle bundle) {
+        for (int i = mClients.size() - 1; i >= 0; i--) {
             try {
-                runnable.broadcast(mCallbacks.getBroadcastItem(i));
+                Message message = Message.obtain(null, what);
+                message.setData(bundle);
+                mClients.get(i).send(message);
             } catch (RemoteException e) {
-                // The RemoteCallbackList will take care of removing
-                // the dead object for us.
+                // The client is dead.  Remove it from the list;
+                // we are going through the list from back to front
+                // so this is safe to do inside the loop.
+                mClients.remove(i);
             }
         }
-        mCallbacks.finishBroadcast();
     }
 }
